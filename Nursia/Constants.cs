f55b19e170c3799e5b1785a86a4bca6e76ddb25a
Roman Shapiro <eleroman@mail.ru>
2024-10-02 15:35:20 +0700

    Closed #47

--- Nursia/Constants.cs
+++ Nursia/Constants.cs
@@ -4,7 +4,7 @@
 	{
 		public const int MaximumBones = 96;
 		public const int MaxLights = 32;
-		public const float ShadowMaxDistance = 200.0f;
+		public const float ShadowMaxDistance = 300.0f;
 		public const int ShadowMapCascadesPerRow = 2;
 		public const int ShadowMapCascadesCount = 4;
 		public const float ShadowMapCascadeSplitLambda = 0.5f;
